1ba9d2484cd00983316f4f051f475f159c2b0ecb
mosesh12see <gosunlite@gmail.com>
2026-05-13 13:59:21 -0500

    Schedule: add Josh Pena to solar-exits

--- scripts/generate-config.js
+++ scripts/generate-config.js
@@ -46,6 +46,7 @@ const ROSTERS = {
     { slug: 'joshua',    name: 'Joshua S Drillette',  defaultTz: 'America/Chicago' },
     { slug: 'jeff',      name: 'Jeff Pearson',        defaultTz: 'America/Chicago' },
     { slug: 'diogo',     name: 'Diogo Correa',        defaultTz: 'America/Chicago' },
+    { slug: 'jpena',     name: 'Josh Pena',           defaultTz: 'America/Chicago' },
   ],
   'capital-energy-az': [
     { slug: 'dayne',     name: 'Dayne Hall',          defaultTz: 'America/Phoenix' },
